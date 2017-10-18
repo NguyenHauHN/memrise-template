3e38972e223f3e9755680b8a585d2238e9985e97
HauNguyen <nguyenhau1996mta@gmail.com>
2017-10-18 22:30:39 +0700

    garden page

--- js/garden.js
+++ js/garden.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-   $("#audio").click(function () {
-       $(this).toggleClass("icon-play icon-paused");
-   });
+$(document).ready(function () {
+    $("#audio").click(function () {
+        $(this).toggleClass("icon-play icon-paused");
+    });
 
-   $(".sound-button").click(function () {
-       $(this).find("#turn-off").toggleClass("block hide");
-       $(this).find("#turn-on").toggleClass("hide block");
-   });
+    $(".sound-button").click(function () {
+        $(this).find("#turn-off").toggleClass("block hide");
+        $(this).find("#turn-on").toggleClass("hide block");
+    });
+
+    $("button.answer-item").click(function () {
+        var parent = $(this).closest(".answer-row");
+        var listAnswerItem = $(parent).find(".answer-item");
+        $(this).removeClass("choosed").addClass("correct");
+        $.each(listAnswerItem, function (idx, item) {
+            if (!$(item).hasClass("incorrect") && !$(item).hasClass("correct")) {
+               $(item).addClass("disable");
+            }
+        });
+    });
 });
